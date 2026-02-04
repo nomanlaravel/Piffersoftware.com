d6617f70db33bf477690d8be818f73a8d90fca15
Nicers <ata10plus10@gmail.com>
2026-02-04 09:33:51 +0500

    1. Monthly Non working days migration database table schema setuped.

---
+++ database/migrations/2026_02_03_124710_monthly_non_working_days.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('monthly_non_working_days', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->year('year');
+            $table->tinyInteger('month');
+            $table->date('date');
+            $table->boolean('is_working_day')->default(true);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unique(['date']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        
+        Schema::dropIfExists('monthly_non_working_days');
+    }
+};
